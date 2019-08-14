87bc8733873826ee066149c2a5a54970858a7019
Hassan1901f <hassan1901f@aptechorangi.com>
2019-08-14 10:28:42 +0500

    First comit

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+function togglemenu1(){
+    document.getElementById('navbar').classList.toggle('active');
+}
+function togglemenu2(){
+    document.getElementById('navbar-L').classList.toggle('active');
+}
+function togglemenu3(){
+    document.getElementById('navbar').classList.toggle('active');
+}
